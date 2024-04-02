f4c2ddfe930cd80806534d1e6fea8a0c49ad8362
pizzamoya <151932541+pizzamoya@users.noreply.github.com>
2024-04-02 10:51:04 +0300

    BR-R.sh

--- BR-R.sh
+++ BR-R.sh
@@ -103,9 +103,11 @@ echo "nameserver 77.88.8.8" >> /etc/resolv.conf
  
 useradd branch-admin -m -c "Branch admin" -U
 passwd branch-admin
+echo -e "P@ssw0rd\nP@ssw0rd" | passwd branch-admin
 
 useradd network-admin -m -c "Network admin" -U
 passwd network-admin
+echo -e "P@ssw0rd\nP@ssw0rd" | passwd network-admin
 
 chmod +x /root/momo/backup.sh
 sh /root/momo/backup.sh
